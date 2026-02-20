ea0c74172ddbdd0462d9efd5af76282298bb5e08
grangermm311 <grangermm311@gmail.com>
2026-02-20 14:53:50 +0000

    update_data-product

---
+++ script.js
@@ -0,0 +1,168 @@
+      const detail_product = document.querySelector(".detail-product");
+      
+      let data_product = [
+          {
+              id: 1,
+              nama: "Ardiles-10 pro",
+              kategori: "Sport",
+              price: 1500000,
+              image:"",
+              size: ["39", "40", "41", "42", "43"]
+          },
+          {
+              id: 2,
+              nama: "Eiger-Raptor",
+              kategori: "Mountain Style",
+              price: 2500000,
+              image: "",
+              size: ["40", "41", "42", "43", "44"]
+          },
+          {
+              id: 3,
+              nama: "Nike-Air Max",
+              kategori: "Sport",
+              price: 1800000,
+              image: "",
+              size: ["39", "40", "41", "42"]
+          },
+          {
+              id: 4,
+              nama: "Adidas-Predator",
+              kategori: "Sport",
+              price: 2200000,
+              image: "",
+              size: ["40", "41", "42", "43"]
+          },
+          {
+              id: 5,
+              nama: "Reebok-Classic",
+              kategori: "Professional",
+              price: 1200000,
+              image: "",
+              size: ["38", "39", "40", "41"]
+          },
+          {
+              id: 6,
+              nama: "Vans-Old Skool",
+              kategori: "Sport",
+              price: 950000,
+              image: "",
+              size: ["37", "38", "39", "40", "41"]
+          },
+          {
+              id: 7,
+              nama: "Converse-Chuck Taylor",
+              kategori: "Sport",
+              price: 850000,
+              image: "",
+              size: ["36", "37", "38", "39", "40"]
+          },
+          {
+              id: 8,
+              nama: "New Balance-574",
+              kategori: "Sport",
+              price: 1350000,
+              image: "",
+              size: ["39", "40", "41", "42", "43"]
+          },
+          {
+              id: 9,
+              nama: "Puma-Suede",
+              kategori: "Sport",
+              price: 1100000,
+              image: "",
+              size: ["38", "39", "40", "41"]
+          },
+          {
+              id: 10,
+              nama: "Asics-Gel-Kayano",
+              kategori: "Sport",
+              price: 1900000,
+              image: "",
+              size: ["40", "41", "42", "43"]
+          },
+          {
+              id: 11,
+              nama: "Salomon-Speedcross",
+              kategori: "Mountain Style",
+              price: 2800000,
+              image: "",
+              size: ["41", "42", "43", "44"]
+          },
+          {
+              id: 12,
+              nama: "Merrell-Moab",
+              kategori: "Mountain Style",
+              price: 2650000,
+              image: "",
+              size: ["40", "41", "42", "43"]
+          },
+          {
+              id: 13,
+              nama: "La Sportiva-Tarantulace",
+              kategori: "Mountain Style",
+              price: 3100000,
+              image: "",
+              size: ["39", "40", "41", "42"]
+          },
+          {
+              id: 14,
+              nama: "Bata-Professional",
+              kategori: "Professional",
+              price: 750000,
+              image: "",
+              size: ["39", "40", "41", "42", "43"]
+          },
+          {
+              id: 15,
+              nama: "Clarks-Desert Boot",
+              kategori: "Professional",
+              price: 1600000,
+              image: "",
+              size: ["39", "40", "41"]
+          },
+          {
+              id: 16,
+              nama: "Dr. Martens-1460",
+              kategori: "Professional",
+              price: 2100000,
+              image: "",
+              size: ["38", "39", "40", "41", "42"]
+          },
+          {
+              id: 17,
+              nama: "Fila-Disruptor",
+              kategori: "Sport",
+              price: 1050000,
+              image: "",
+              size: ["37", "38", "39", "40"]
+          },
+          {
+              id: 18,
+              nama: "Under Armour-HOVR Phantom",
+              kategori: "Sport",
+              price: 1750000,
+              image: "",
+              size: ["40", "41", "42", "43"]
+          },
+          {
+              id: 19,
+              nama: "Skechers-Go Walk",
+              kategori: "Sport",
+              price: 900000,
+              image: "",
+              size: ["38", "39", "40", "41", "42"]
+          },
+          {
+              id: 20,
+              nama: "Ardiles-20 pro",
+              kategori: "Sport",
+              price: 1600000,
+              image: "",
+              size: ["39", "40", "41", "42", "43"]
+          }
+      ];
+      
+      function show_proudct(params) {
+          
+      }
